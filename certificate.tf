837d39e5aded19a01406d1eb55b0c77effd0e46c
etwillbefine <tim.gatzemeier@gmx.de>
2020-09-10 18:49:12 +0200

    correct variable name for acm sans

--- certificate.tf
+++ certificate.tf
@@ -3,7 +3,7 @@ resource "aws_acm_certificate" "cloudfront" {
   domain_name               = var.cloudfront_domain == "" ? var.dns_zone_name : format("%s.%s", var.cloudfront_domain, var.dns_zone_name)
   validation_method         = "DNS"
   tags                      = module.cdn_label.tags
-  subject_alternative_names = var.subject_alternative_dns.*.name
+  subject_alternative_names = var.acm_subject_alternative_dns.*.name
 
   lifecycle {
     create_before_destroy = true
